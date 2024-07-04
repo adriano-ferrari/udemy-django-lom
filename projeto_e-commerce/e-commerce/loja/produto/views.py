11b69f2a7c424625e0694f6e0d4fa9d38d9207e3
adriano-ferrari <irarrefonairda@gmail.com>
2024-07-04 10:26:47 -0300

    Aula 572 - Detalhes dos produtos

--- projeto_e-commerce/e-commerce/loja/produto/views.py
+++ projeto_e-commerce/e-commerce/loja/produto/views.py
@@ -12,13 +12,15 @@ class ListaProdutos(ListView):
     model = models.Produto
     template_name = 'produto/lista.html'
     context_object_name = 'produtos'
-    paginate_by = 10
+    paginate_by = 5
     ordering = ['-id']
 
 
 class DetalheProduto(DetailView):
-    def get(self, *args, **kwargs):
-        return HttpResponse('Detalhe')
+    model = models.Produto
+    template_name = 'produto/detalhe.html'
+    context_object_name = 'produto'
+    slug_url_kwarg = 'slug'
 
 
 class AdicionarAoCarrinho(View):
