b67c05cefc82b5a210cb672f1d36959192ff0609
adriano-ferrari <irarrefonairda@gmail.com>
2024-07-06 19:38:57 -0300

    Aula 579 - Cadastro de Usuarios (Parte 2)

--- projeto_e-commerce/e-commerce/loja/perfil/views.py
+++ projeto_e-commerce/e-commerce/loja/perfil/views.py
@@ -1,6 +1,10 @@
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views import View
+from django.contrib.auth.models import User
+import copy
 
 from . import models
 from . import forms
@@ -11,6 +15,10 @@ class BasePerfil(View):
 
     def setup(self, *args, **kwargs):
         super().setup(*args, **kwargs)
+
+        self.carrinho = copy.deepcopy(self.request.session.get('carrinho', {}))
+
+        self.perfil = None
 
         if self.request.user.is_authenticated:
             self.perfil = models.Perfil.objects.filter(
@@ -38,11 +46,11 @@ class BasePerfil(View):
                 )
             }
 
-        self.contexto = {
-            'userform': forms.UserForm(
-                data=self.request.POST or None
-            ),
-        }
+        self.userform = self.contexto['userform']
+        self.perfilform = self.contexto['perfilform']
+
+        if self.request.user.is_authenticated:
+            self.template_name = 'perfil/atualizar.html'
 
         self.renderizar = render(
             self.request, self.template_name, self.contexto)
@@ -52,7 +60,82 @@ class BasePerfil(View):
 
 
 class Criar(BasePerfil):
-    pass
+    def post(self, *args, **kwargs):
+        if not self.userform.is_valid() or not self.perfilform.is_valid():
+            messages.error(
+                self.request,
+                'Existem erros no formulário de cadastro. Verifique se todos '
+                'os campos foram preenchidos corretamente.'
+            )
+
+            return self.renderizar
+
+        username = self.userform.cleaned_data.get('username')
+        password = self.userform.cleaned_data.get('password')
+        email = self.userform.cleaned_data.get('email')
+        first_name = self.userform.cleaned_data.get('first_name')
+        last_name = self.userform.cleaned_data.get('last_name')
+
+        # Usuário logado
+        if self.request.user.is_authenticated:
+            usuario = get_object_or_404(
+                User, username=self.request.user.username)
+
+            usuario.username = username
+
+            if password:
+                usuario.set_password(password)
+
+            usuario.email = email
+            usuario.first_name = first_name
+            usuario.last_name = last_name
+            usuario.save()
+
+            if not self.perfil:
+                self.perfilform.cleaned_data['usuario'] = usuario
+                print(self.perfilform.cleaned_data)
+                perfil = models.Perfil(**self.perfilform.cleaned_data)
+                perfil.save()
+            else:
+                perfil = self.perfilform.save(commit=False)
+                perfil.usuario = usuario
+                perfil.save()
+
+        # Usário não logado (novo)
+        else:
+            usuario = self.userform.save(commit=False)
+            usuario.set_password(password)
+            usuario.save()
+
+            perfil = self.perfilform.save(commit=False)
+            perfil.usuario = usuario
+            perfil.save()
+
+        if password:
+            autentica = authenticate(
+                self.request,
+                username=usuario,
+                password=password
+            )
+
+            if autentica:
+                login(self.request, user=usuario)
+
+        self.request.session['carrinho'] = self.carrinho
+        self.request.session.save()
+
+        messages.success(
+            self.request,
+            'Seu cadastro foi criado ou atualizado com sucesso.'
+        )
+
+        messages.success(
+            self.request,
+            'Você fez login e pode concluir sua compra.'
+        )
+
+        return redirect('produto:carrinho')
+        return self.renderizar
 
 
 class Atualizar(View):
