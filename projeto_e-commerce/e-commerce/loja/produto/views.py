5d13c4fa252ff91c60b31284f8ff5f81b73146a3
adriano-ferrari <irarrefonairda@gmail.com>
2024-07-06 09:25:58 -0300

    Aula 574 - Adicionar ao carrinho - parte 2

--- projeto_e-commerce/e-commerce/loja/produto/views.py
+++ projeto_e-commerce/e-commerce/loja/produto/views.py
@@ -39,6 +39,29 @@ class AdicionarAoCarrinho(View):
             return redirect(http_referer)
 
         variacao = get_object_or_404(models.Variacao, id=variacao_id)
+        variacao_estoque = variacao.estoque
+        produto = variacao.produto
+
+        produto_id = produto.id
+        produto_nome = produto.nome
+        variacao_nome = variacao.nome or ''
+        preco_unitario = variacao.preco
+        preco_unitario_promocional = variacao.preco_promocional
+        quantidade = 1
+        slug = produto.slug
+        imagem = produto.imagem
+
+        if imagem:
+            imagem = imagem.name
+        else:
+            imagem = ''
+
+        if variacao.estoque < 1:
+            messages.error(
+                self.request,
+                'Estoque insuficiente'
+            )
+            return redirect(http_referer)
 
         if not self.request.session.get('carrinho'):
             self.request.session['carrinho'] = {}
@@ -46,7 +69,48 @@ class AdicionarAoCarrinho(View):
 
         carrinho = self.request.session['carrinho']
 
-        return HttpResponse(f'{variacao.produto} {variacao.nome}')
+        if variacao_id in carrinho:
+            quantidade_carrinho = carrinho[variacao_id]['quantidade']
+            quantidade_carrinho += 1
+
+            if variacao_estoque < quantidade_carrinho:
+                messages.warning(
+                    self.request,
+                    f'Estoque insuficiente para {quantidade_carrinho}x no '
+                    f'produto "{produto_nome}". Adicionamos {variacao_estoque}x '
+                    f'no seu carrinho.'
+                )
+                quantidade_carrinho = variacao_estoque
+
+            carrinho[variacao_id]['quantidade'] = quantidade_carrinho
+            carrinho[variacao_id]['preco_quantitativo'] = preco_unitario * \
+                quantidade_carrinho
+            carrinho[variacao_id]['preco_quantitativo_promocional'] = preco_unitario_promocional * \
+                quantidade_carrinho
+        else:
+            carrinho[variacao_id] = {
+                'produto_id': produto_id,
+                'produto_nome': produto_nome,
+                'variacao_nome': variacao_nome,
+                'variacao_id': variacao_id,
+                'preco_unitario': preco_unitario,
+                'preco_unitario_promocional': preco_unitario_promocional,
+                'preco_quantitativo': preco_unitario,
+                'preco_quantitativo_promocional': preco_unitario_promocional,
+                'quantidade': 1,
+                'slug': slug,
+                'imagem': imagem,
+            }
+
+        self.request.session.save()
+
+        messages.success(
+            self.request,
+            f'Produto {produto_nome} {variacao_nome} adicionado ao seu '
+            f'carrinho {carrinho[variacao_id]["quantidade"]} X.'
+        )
+
+        return redirect(http_referer)
 
 
 class RemoverDoCarrinho(View):
@@ -56,7 +120,11 @@ class RemoverDoCarrinho(View):
 
 class Carrinho(View):
     def get(self, *args, **kwargs):
-        return HttpResponse('Carrinho')
+        contexto = {
+            'carrinho': self.request.session.get('carrinho', {})
+        }
+
+        return render(self.request, 'produto/carrinho.html', contexto)
 
 
 class ResumoDaCompra(View):
