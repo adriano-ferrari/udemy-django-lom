5196e58550255a03d2a840b4008617e47304c7d8
adriano-ferrari <irarrefonairda@gmail.com>
2024-07-07 12:46:16 -0300

    Aula 582 - Resumo da compra

--- projeto_e-commerce/e-commerce/loja/produto/views.py
+++ projeto_e-commerce/e-commerce/loja/produto/views.py
@@ -5,6 +5,8 @@ from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.contrib import messages
 
+from ..perfil.models import Perfil
+
 from . import models
 
 
